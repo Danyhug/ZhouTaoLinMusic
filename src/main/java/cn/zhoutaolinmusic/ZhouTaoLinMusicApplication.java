010ed849cd4114f4ef3985a1eeeb0f83e737f652
Danyhug <danyhug@qq.com>
2024-07-18 13:02:21 +0800

    init

---
+++ src/main/java/cn/zhoutaolinmusic/ZhouTaoLinMusicApplication.java
@@ -0,0 +1,11 @@
+package cn.zhoutaolinmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhouTaoLinMusicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZhouTaoLinMusicApplication.class, args);
+    }
+}
---
+++ src/main/java/cn/zhoutaolinmusic/controller/LoginController.java
@@ -0,0 +1,9 @@
+package cn.zhoutaolinmusic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jjjmusic/login")
+public class LoginController {
+}
