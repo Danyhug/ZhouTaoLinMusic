38780a415a05644e3e605045a40f9aff8785f9b9
Danyhug <danyhug@qq.com>
2024-07-30 14:52:55 +0800

    add 角色管理

---
+++ src/main/java/cn/zhoutaolinmusic/controller/admin/RoleController.java
@@ -0,0 +1,107 @@
+package cn.zhoutaolinmusic.controller.admin;
+
+import cn.zhoutaolinmusic.authority.Authority;
+import cn.zhoutaolinmusic.entity.user.Role;
+import cn.zhoutaolinmusic.entity.user.Tree;
+import cn.zhoutaolinmusic.entity.user.UserRole;
+import cn.zhoutaolinmusic.entity.vo.AssignRoleVO;
+import cn.zhoutaolinmusic.entity.vo.BasePage;
+import cn.zhoutaolinmusic.service.user.RolePermissionService;
+import cn.zhoutaolinmusic.service.user.RoleService;
+import cn.zhoutaolinmusic.service.user.UserRoleService;
+import cn.zhoutaolinmusic.utils.Result;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/authorize/role")
+public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private RolePermissionService rolePermissionService;
+
+    @Autowired
+    private UserRoleService userRoleService;
+
+    /**
+     * 获取用户角色
+     * @param userId
+     * @return
+     */
+    @GetMapping("/getUserRole/{userId}")
+    @Authority("role:getRole")
+    public List getRole(@PathVariable Integer userId){
+        List<Long> list = userRoleService.list(new LambdaQueryWrapper<UserRole>().eq(UserRole::getUserId,userId).select(UserRole::getRoleId))
+                .stream().map(UserRole::getRoleId).collect(Collectors.toList());
+        return list;
+    }
+
+    /**
+     * 初始化角色
+     * @return
+     */
+    @GetMapping("/initRole")
+    @Authority("role:initRole")
+    public List<Map<String, Object>> initRole(){
+        // 查出所有角色
+        List<Map<String, Object>> list = roleService.list(null).stream()
+                .map(role -> {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("value", role.getId());
+                    data.put("title", role.getName());
+                    return data;
+                }).collect(Collectors.toList());
+
+        return list;
+    }
+
+    /**
+     * 分配角色
+     * @param assignRoleVO
+     * @return
+     */
+    @PostMapping("/assignRole")
+    @Authority("role:assignRole")
+    public Result assignRole(@RequestBody AssignRoleVO assignRoleVO) {
+        return roleService.gaveRole(assignRoleVO);
+    }
+
+    /**
+     * 获取角色树
+     * @return
+     */
+    @GetMapping("/treeList")
+    @Authority("permission:treeList")
+    public List<Tree> treeList(){
+        List<Tree> data = roleService.tree();
+        return data;
+    }
+
+    /**
+     * 获取角色列表
+     * @param basePage
+     * @param name
+     * @return
+     */
+    @GetMapping("/list")
+    @Authority("role:list")
+    public Result list(BasePage basePage, @RequestParam(required = false) String name) {
+        LambdaQueryWrapper<Role> wrapper = new LambdaQueryWrapper<>();
+        wrapper.like(!ObjectUtils.isEmpty(name), Role::getName, name);
+
+        IPage iPage = basePage.page();
+        IPage page = roleService.page(iPage, wrapper);
+        return Result.ok(page.getRecords(), page.getTotal());
+    }
+}
--- src/main/java/cn/zhoutaolinmusic/service/user/impl/RoleServiceImpl.java
+++ src/main/java/cn/zhoutaolinmusic/service/user/impl/RoleServiceImpl.java
@@ -1,22 +1,76 @@
 package cn.zhoutaolinmusic.service.user.impl;
 
+import cn.zhoutaolinmusic.authority.Authority;
 import cn.zhoutaolinmusic.entity.user.Role;
 import cn.zhoutaolinmusic.entity.user.Tree;
+import cn.zhoutaolinmusic.entity.user.UserRole;
 import cn.zhoutaolinmusic.entity.vo.AssignRoleVO;
 import cn.zhoutaolinmusic.entity.vo.AuthorityVO;
 import cn.zhoutaolinmusic.mapper.user.RoleMapper;
+import cn.zhoutaolinmusic.service.user.PermissionService;
 import cn.zhoutaolinmusic.service.user.RoleService;
+import cn.zhoutaolinmusic.service.user.UserRoleService;
 import cn.zhoutaolinmusic.utils.Result;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements RoleService {
+
+    @Autowired
+    private UserRoleService userRoleService;
+
+    @Autowired
+    private PermissionService permissionService;
+
     @Override
     public List<Tree> tree() {
-        return null;
+        // 获取权限列表的所有数据
+        List<Tree> trees = permissionService.list(null).stream().map(permission -> {
+            Tree tree = new Tree();
+            BeanUtils.copyProperties(permission, tree);
+            tree.setTitle(permission.getName());
+            tree.setSpread(true);
+            return tree;
+        }).collect(Collectors.toList());
+
+        // 找到根节点
+        List<Tree> parent = trees.stream().filter(tree -> tree.getPId().compareTo(0L) == 0).collect(Collectors.toList());
+        for (Tree item: parent) {
+            // 生成树
+            item.setChildren(new ArrayList<>());
+            item.getChildren().add(findChildren(item, trees));
+        }
+
+        return parent;
+    }
+
+    /**
+     * 递归查询子节点
+     * @param datum 当前元素
+     * @param trees 所有数据
+     * @return
+     */
+    private Tree findChildren(Tree datum, List<Tree> trees) {
+        datum.setChildren(new ArrayList<>());
+        for (Tree tree: trees) {
+            // 判断当前元素的id是否等于 tree 的pId （当前元素是否为他的父元素）
+            if (tree.getPId().compareTo(datum.getId()) == 0) {
+                // 说明tree是datum的子元素
+                datum.getChildren().add(findChildren(tree, trees));
+            }
+        }
+        return datum;
     }
 
     @Override
@@ -30,7 +84,27 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements Ro
     }
 
     @Override
+    @Transactional
     public Result gaveRole(AssignRoleVO assignRoleVO) {
-        return null;
+        try {
+            // 获取用户信息
+            Long uId = assignRoleVO.getUId();
+            // 先删除所对应用户的所有角色
+            userRoleService.remove(new LambdaQueryWrapper<UserRole>().eq(UserRole::getUserId, uId));
+            List<UserRole> userRoles = new ArrayList<>();
+            for (Long id: assignRoleVO.getRId()) {
+                UserRole userRole = new UserRole();
+                userRole.setRoleId(id);
+                userRole.setUserId(uId);
+                userRoles.add(userRole);
+            }
+            userRoleService.saveBatch(userRoles);
+        } catch (Exception e) {
+            // 设置事务回滚
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            return Result.error("分配权限失败");
+        }
+
+        return Result.ok("分配权限成功");
     }
 }
